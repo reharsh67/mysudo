012becd987a165fdf10d928040c051c11d2e19c2
MASTERGUY <murtipipo@gmail.com>
2020-03-10 11:19:52 +0530

    mysudo add aex support

--- mysudo.sh
+++ mysudo.sh
@@ -4,7 +4,9 @@ echo "Select the base : "
 echo "1. Clone DerpFest DT+Kernel+CDT+Vendor From Official Sources"
 echo "2. Clone Du-Rex DT+Kernel+CDT+Vendor"
 echo "3. Clone DerpFest DT+Kernel+CDT+Vendor From My Own Repo"
-echo "4. Start the build ;) "
+echo "4. Clone AEX DT+Kernel+CDT From Official Sources"
+echo "5. Start the build DerpFest ;) "
+echo "6. Start the build AEX ;) "
 read base
 
 if [ $base = 1 ]
@@ -13,7 +15,6 @@ git clone https://github.com/DerpFest-Devices/device_xiaomi_tissot.git device/xi
 git clone https://github.com/DerpFest-Devices/device_xiaomi_msm8953-common.git device/xiaomi/msm8953-common
 git clone https://github.com/DerpFest-Devices/kernel_xiaomi_msm8953.git kernel/xiaomi/msm8953
 git clone https://github.com/DerpFest-Devices/vendor_xiaomi_tissot.git vendor/xiaomi
-git clone https://github.com/MASTERGUY/android_device_twrp_tissot.git device/twrp/tissot
 echo "Removing Old Gapps & Replacing it with mine!"
 rm -rf vendor/gapps/config.mk
 wget -P vendor/gapps/ https://raw.githubusercontent.com/MASTERGUY/android_vendor_gapps/master/config.mk
@@ -29,7 +30,6 @@ git clone https://github.com/MASTERGUY/android_device_xiaomi_tissot.git -b du10 
 git clone https://github.com/MASTERGUY/android_device_xiaomi_msm8953-common.git device/xiaomi/msm8953-common
 git clone https://github.com/MASTERGUY/kernel_xiaomi_msm8953.git kernel/xiaomi/msm8953
 git clone https://github.com/MASTERGUY/proprietary_vendor_xiaomi.git vendor/xiaomi
-git clone https://github.com/MASTERGUY/android_device_twrp_tissot.git device/twrp/tissot
 fi
 
 if [ $base = 3 ]
@@ -38,8 +38,7 @@ git clone https://github.com/MASTERGUY/android_device_xiaomi_tissot.git device/x
 git clone https://github.com/MASTERGUY/android_device_xiaomi_msm8953-common.git device/xiaomi/msm8953-common
 git clone https://github.com/MASTERGUY/kernel_xiaomi_msm8953.git kernel/xiaomi/msm8953
 git clone https://github.com/MASTERGUY/proprietary_vendor_xiaomi.git vendor/xiaomi
-git clone https://github.c"om/MASTERGUY/android_device_twrp_tissot.git device/twrp/tissot
-echo "Removing Old Gapps & Replacing it with mine!
+echo "Removing Old Gapps & Replacing it with mine"!
 rm -rf vendor/aosip/gapps/config.mk
 wget -P vendor/gapps/ https://raw.githubusercontent.com/MASTERGUY/android_vendor_gapps/master/config.mk
 echo "Adding FM From LOS"
@@ -48,6 +47,14 @@ git clone https://github.com/LineageOS/android_vendor_qcom_opensource_libfmjni.g
 fi
 
 if [ $base = 4 ]
+then
+git clone https://github.com/AospExtended-Devices/device_xiaomi_tissot.git.git -b aex device/xiaomi/tissot
+git clone https://github.com/AospExtended-Devices/device_xiaomi_msm8953-common.git -b aex device/xiaomi/msm8953-common
+git clone https://github.com/AospExtended-Devices/kernel_xiaomi_msm8953.git -b ten kernel/xiaomi/msm8953
+git clone https://github.com/DerpFest-Devices/vendor_xiaomi_tissot.git vendor/xiaomi
+fi
+
+if [ $base = 5 ]
 then
 rm -rf vendor/aosip/gapps/config.mk
 rm -rf vendor/pixelstyle/
@@ -62,4 +69,13 @@ git clone https://github.com/MASTERGUY/platform_vendor_pixelstyle.git vendor/pix
 lunch derp_tissot-userdebug
 time mka kronic
 fi
-echo "Script finished its job"
+
+if [ $base = 6 ]
+then
+repo sync --force-sync --no-tags --no-clone-bundle
+. build/envsetup.sh
+lunch aosp_tissot-userdebug
+mka aex -j$(nproc --all)
+fi
+
+echo "Task is executed successfully"
