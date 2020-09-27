5419e86de1d1dddbc29d83d2eba7bd44d43c9a6b
Murtaza@Piplod <murtaza1926@gmail.com>
2020-09-27 12:18:28 +0530

    Update mysudo.sh

--- mysudo.sh
+++ mysudo.sh
@@ -148,7 +148,7 @@ then
 rm -rf hardware/qcom-caf/msm8996/display hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/media
 git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/display
 git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/media
-git clone https://github.com/Line ageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
 fi
 
 if [ $base = 13 ]
